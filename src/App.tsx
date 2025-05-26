f0f2a4e3ff12f5792296aa55bb9cf840624d659d
VitorMachadoSilva <vitor.machado.silva05@gmail.com>
2025-05-26 11:47:35 -0300

    add countdown and menu icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Container } from "./components/Container";
-import { Heading } from "./components/Heading";
+import { CountDown } from "./components/CountDown";
 import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 import "./styles/global.css";
 import "./styles/theme.css";
@@ -12,7 +13,10 @@ export function App() {
         <Logo />
       </Container>
       <Container>
-        <Heading>MENU</Heading>
+        <Menu/>
+      </Container>
+      <Container>
+        <CountDown/>
       </Container>
     </>
   );
