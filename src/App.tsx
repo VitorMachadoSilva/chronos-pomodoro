99cb7f78e36c107b1b2dd6ab4bedff4dbd06f7f0
VitorMachadoSilva <vitor.machado.silva05@gmail.com>
2025-06-09 20:15:06 -0300

    adicionar ciclos e botões

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { PlayCircleIcon } from "lucide-react";
 import { Container } from "./components/Container";
 import { CountDown } from "./components/CountDown";
+import { Cycles } from "./components/Cycles";
+import { DefaultButton } from "./components/DefaultButton";
+import { DefaultInput } from "./components/DefaultInput";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 
@@ -12,11 +16,39 @@ export function App() {
       <Container>
         <Logo />
       </Container>
+
       <Container>
         <Menu/>
       </Container>
+
       <Container>
         <CountDown/>
+      </Container>
+
+      <Container>
+        <form className="form" action=''>
+          <div className="formRow">
+            <DefaultInput 
+            id="meuInput" 
+            type="text" 
+            labelText="task" 
+            placeholder="Digite algo"
+            />
+          </div>
+
+          <div className='formRow'>Add commentMore actions
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles/>
+          </div>
+
+          <div className='formRow'>
+            <DefaultButton icon={<PlayCircleIcon/>} color="green"/>
+            <DefaultButton icon={<PlayCircleIcon/>} color="red"/>
+          </div>
+        </form>
       </Container>
     </>
   );
